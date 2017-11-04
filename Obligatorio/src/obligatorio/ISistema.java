ce6740c88b3bbbc1e85779b8511ee61f0d81b66d
Polachek <guillermollana@gmail.com>
2017-11-04 15:47:31 -0300

    Base

---
+++ Obligatorio/src/obligatorio/ISistema.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obligatorio;
+
+/**
+ *
+ * @author User
+ */
+public interface ISistema {
+    Retorno crearSistemaReservas(int cantCiudades);
+	Retorno destruirSistemaReservas();
+	Retorno registrarCiudad(String ciudad);
+	Retorno registrarCrucero(String ciudad, String nombre, int estrellas,int capacidad);
+	Retorno ingresarServicio(String ciudad, String crucero, String servicio);
+	Retorno borrarServicio(String ciudad, String crucero, String servicio);
+	Retorno realizarReserva(int cliente, String ciudad, String crucero);
+	Retorno cancelarReserva(int cliente, String ciudad, String crucero);
+	Retorno ingresarComentario(String ciudad, String crucero, String comentario, int ranking);
+	Retorno listarServicios(String ciudad, String crucero);
+	Retorno listarCrucerosCiudad(String ciudad);
+	Retorno listarCrucerosRankingAsc(String ciudad);
+	Retorno listarCrucerosRankingDesc(String ciudad);
+	Retorno listarCrucerosRanking();
+	Retorno listarComentarios(String ciudad, String crucero);
+	Retorno cargarDistancias(int[][] ciudades);
+	Retorno buscarCamino(int [][] m, String origen, String destino);
+}
