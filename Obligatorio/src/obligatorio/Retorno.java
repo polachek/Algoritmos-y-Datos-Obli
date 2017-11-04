3f1771a18db3d00bc5c4ed03db87b6e0d4080c4f
SebastianVillar <sebastian.villar@outlook.com>
2017-11-04 15:58:47 -0300

    Tabulaciones

--- Obligatorio/src/obligatorio/Retorno.java
+++ Obligatorio/src/obligatorio/Retorno.java
@@ -11,10 +11,9 @@ package obligatorio;
  */
 public class Retorno {
     enum Resultado {
-		OK, ERROR_1, ERROR_2, ERROR_3, ERROR_4, ERROR_5, NO_IMPLEMENTADA
-	};
-
-	int valorEntero;
-	String valorString;
-	Resultado resultado;
+        OK, ERROR_1, ERROR_2, ERROR_3, ERROR_4, ERROR_5, NO_IMPLEMENTADA
+    };
+    int valorEntero;
+    String valorString;
+    Resultado resultado;
 }
